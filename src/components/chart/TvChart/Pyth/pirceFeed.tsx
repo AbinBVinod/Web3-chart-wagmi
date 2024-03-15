4a9e6cffe6232b83e9e1190d7bc4d4738a1b57f6
AbinBVinod <abinbvinod@gmail.com>
2024-03-15 12:26:50 +0530

    time

--- src/components/chart/TvChart/Pyth/pirceFeed.tsx
+++ src/components/chart/TvChart/Pyth/pirceFeed.tsx
@@ -48,7 +48,7 @@ const PriceFeedDisplay = () => {
       }
     };
 
-    const interval = setInterval(fetchData); 
+    const interval = setInterval(fetchData , 1000); 
 
     return () => clearInterval(interval);
   }, []);
